c6bfbfa7634133e38733aae0ef0ed77b6ad239ac
Mitch Wilson <mitchwilson1969@gmail.com>
2021-05-25 12:54:02 -0500

    Setup sass for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -25,15 +25,8 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-        ],
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
     ],
   },
